80b04cb3b9a3c89d586bb2f370365e010c4fe8d2
santibaezagraf <santibaezagraf@gmail.com>
2025-09-17 11:44:49 -0300

    Please enter the commit message for your changes. stash

--- exercises/9- Next steps/next-steps/src/test/setup.ts
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-import { expect, afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
-
-// Limpiar después de cada test
-afterEach(() => {
-    cleanup();
-});
